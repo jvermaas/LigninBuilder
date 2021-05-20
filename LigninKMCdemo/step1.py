fd792f6c0b4d695a6b1829a0f301e0bdd4467d09
Josh Vermaas <vermaasjv@ornl.gov>
2021-05-20 09:34:25 -0400

    Update to using the new ligninkmc API

--- LigninKMCdemo/step1.py
+++ LigninKMCdemo/step1.py
@@ -1,94 +1,46 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from ligninkmc.Visualization import generatePsfgen
-import ligninkmc as kmc
+from ligninkmc.kmc_functions import (run_kmc, generate_mol, gen_tcl)
+from ligninkmc.create_lignin import (calc_rates, create_initial_monomers, create_initial_events,
+                                     create_initial_state, analyze_adj_matrix, adj_analysis_to_stdout)
+from ligninkmc.kmc_common import (DEF_E_BARRIER_KCAL_MOL, ADJ_MATRIX, MONO_LIST, MONOMER, OX, GROW, C, Monomer, Event)
 import numpy as np
 
-kb = 1.38064852e-23  # J / K
-h = 6.62607004e-34  # J s
 temp = 298.15  # K
-kcalToJoule = 4184 / 6.022140857e23  # J/molecule from kcal/mol; ligninkmc uses Joules
+rxn_rates = calc_rates(temp, ea_kcal_mol_dict=DEF_E_BARRIER_KCAL_MOL)
 
-# Input energy information for ligninkmc; from Michael (KMC)
-# Select which energy set you want to use
-# 0 = G, 1 = S (check in KMC code)
-energies = {'5o4': {(0, 0): {('monomer', 'monomer'): 11.2, ('monomer', 'dimer'): 14.6,
-                             ('dimer', 'monomer'): 14.6, ('dimer', 'dimer'): 4.4},
-                    (1, 0): {('monomer', 'monomer'): 10.9, ('monomer', 'dimer'): 14.6,
-                             ('dimer', 'monomer'): 14.6, ('dimer', 'dimer'): 4.4}},
-            '55': {(0, 0): {('monomer', 'monomer'): 12.5, ('monomer', 'dimer'): 15.6,
-                            ('dimer', 'monomer'): 15.6, ('dimer', 'dimer'): 3.8}},
-            'b5': {(0, 0): {('monomer', 'monomer'): 5.5, ('monomer', 'dimer'): 5.8,
-                            ('dimer', 'monomer'): 5.8, ('dimer', 'dimer'): 5.8},
-                   (0, 1): {('monomer', 'monomer'): 5.5, ('monomer', 'dimer'): 5.8,
-                            ('dimer', 'monomer'): 5.8, ('dimer', 'dimer'): 5.8}},
-            'bb': {(0, 0): {('monomer', 'monomer'): 5.2, ('monomer', 'dimer'): 5.2, ('dimer', 'monomer'): 5.2,
-                            ('dimer', 'dimer'): 5.2},
-                   (1, 0): {('monomer', 'monomer'): 6.5, ('monomer', 'dimer'): 6.5, ('dimer', 'monomer'): 6.5,
-                            ('dimer', 'dimer'): 6.5},
-                   (1, 1): {('monomer', 'monomer'): 5.2, ('monomer', 'dimer'): 5.2, ('dimer', 'monomer'): 5.2,
-                            ('dimer', 'dimer'): 5.2}},
-            'bo4': {(0, 0): {('monomer', 'monomer'): 6.3, ('monomer', 'dimer'): 6.2,
-                             ('dimer', 'monomer'): 6.2, ('dimer', 'dimer'): 6.2},
-                    (1, 0): {('monomer', 'monomer'): 9.1, ('monomer', 'dimer'): 6.2,
-                             ('dimer', 'monomer'): 6.2, ('dimer', 'dimer'): 6.2},
-                    (0, 1): {('monomer', 'monomer'): 8.9, ('monomer', 'dimer'): 6.2,
-                             ('dimer', 'monomer'): 6.2, ('dimer', 'dimer'): 6.2},
-                    (1, 1): {('monomer', 'monomer'): 9.8, ('monomer', 'dimer'): 10.4,
-                             ('dimer', 'monomer'): 10.4}},
-            'ao4': {(0, 0): {('monomer', 'monomer'): 20.7, ('monomer', 'dimer'): 20.7,
-                             ('dimer', 'monomer'): 20.7, ('dimer', 'dimer'): 20.7},
-                    (1, 0): {('monomer', 'monomer'): 20.7, ('monomer', 'dimer'): 20.7,
-                             ('dimer', 'monomer'): 20.7, ('dimer', 'dimer'): 20.7},
-                    (0, 1): {('monomer', 'monomer'): 20.7, ('monomer', 'dimer'): 20.7,
-                             ('dimer', 'monomer'): 20.7, ('dimer', 'dimer'): 20.7},
-                    (1, 1): {('monomer', 'monomer'): 20.7, ('monomer', 'dimer'): 20.7,
-                             ('dimer', 'monomer'): 20.7, ('dimer', 'dimer'): 20.7}},
-            'b1': {(0, 0): {('monomer', 'dimer'): 9.6,
-                            ('dimer', 'monomer'): 9.6, ('dimer', 'dimer'): 9.6},
-                   (1, 0): {('monomer', 'dimer'): 11.7,
-                            ('dimer', 'monomer'): 11.7, ('dimer', 'dimer'): 11.7},
-                   (0, 1): {('monomer', 'dimer'): 10.7,
-                            ('dimer', 'monomer'): 10.7, ('dimer', 'dimer'): 10.7},
-                   (1, 1): {('monomer', 'dimer'): 11.9,
-                            ('dimer', 'monomer'): 11.9, ('dimer', 'dimer'): 11.9}},
-            'ox': {0: {'monomer': 0.9, 'dimer': 6.3}, 1: {'monomer': 0.6, 'dimer': 2.2}},
-            'q': {0: {'monomer': 11.1, 'dimer': 11.1}, 1: {'monomer': 11.7, 'dimer': 11.7}}}
-energies['bb'][(0, 1)] = energies['bb'][(1, 0)]
+# Set the percentage of S
+sg_ratio = 1
+pct_s = sg_ratio / (1 + sg_ratio)
 
-# Correct units of the energies
-energiesev = {bond: {monType: {size: energies[bond][monType][size] * kcalToJoule for size in energies[bond][monType]}
-                     for monType in energies[bond]}
-              for bond in energies}
+# Set the initial and maximum number of monomers to be modeled.
+ini_num_monos = 2
+max_num_monos = 10
 
-# Calculate the rates of reaction
-rates = {bond: {monType: {size: kb * temp / h * np.exp(- energiesev[bond][monType][size] / kb / temp)
-                          for size in energies[bond][monType]}
-                for monType in energies[bond]}  # Rates are in 1/s.
-         for bond in energies}
-# Setup parameters for the simulation
-n = 5  # Number of monomers in the pot initially (arbitrary)
-nMax = 200
-rate = 0.001  # Rate at which monomers are added to the pot; best guess of lignin transport into plant
-tFinal = 3600  # 3600 s = 1 hour (arbitrary)
-sg = 1.4  # S/G ratio  (always change)
-pct = sg / (1 + sg)
-rands = np.random.rand(n)
-mons = [kmc.Monomer(int(sOrG < pct), i) for i, sOrG in zip(range(n), rands)]  # monomers
-startEvents = [kmc.Event('ox', [i], rates['ox'][int(sOrG < pct)]['monomer'])  # ox = oxidize; keep as is
-               for i, sOrG in zip(range(n), rands)]
-state = {i: {'mon': mons[i], 'affected': {startEvents[i]}} for i in range(n)}
-events = {startEvents[i] for i in range(n)}
-events.add(kmc.Event('grow', [], rate=rate, bond=sg))
-results = kmc.run(nMax=nMax, tFinal=tFinal, rates=rates, initialState=state, initialEvents=events, dynamics=False)
-# Some helper print statements to see what is actually *in* the results.
-print(results)
-print(results.keys())
-print(len(results['monomers']))
-print(len(results['adjacency_matrix']))
+# Maximum time to simulate, in seconds
+t_max = 1  # seconds
+mono_add_rate = 1e4  # monomers/second
+
+# Use a random number and the given sg_ratio to determine the monolignol types to be initially modeled
+monomer_draw = np.random.rand(ini_num_monos)
+initial_monomers = create_initial_monomers(pct_s, monomer_draw)
+
+# Initially allow only oxidation events. After they are used to determine the initial state, add 
+#     GROW to the events, which allows additional monomers to be added to the reaction at the 
+#     specified rate and with the specified ratio
+initial_events = create_initial_events(initial_monomers, rxn_rates)
+initial_state = create_initial_state(initial_events, initial_monomers)
+initial_events.append(Event(GROW, [], rate=mono_add_rate))
+
+result = run_kmc(rxn_rates, initial_state,initial_events, n_max=max_num_monos, t_max=t_max, sg_ratio=sg_ratio)
+
+# Convert the sparse matrix to a full array before printing
+print("The adjacency matrix for the simulated lignin is:")
+print(result[ADJ_MATRIX].toarray())
+
 # From the list of monomers and the adjacency matrix, we can use LigninKMC to write out a tcl script for psfgen to
 # turn into a .psf file.
 # fname and sgnames are things that we'd want to change; file name always the same as the segname
-generatePsfgen(results['adjacency_matrix'], results['monomers'], toppardirectory="../smilesdemo/toppar/", fname="psfgen.tcl", segname="L")  
+gen_tcl(result[ADJ_MATRIX], result[MONO_LIST], toppar_dir="../smilesdemo/toppar/", tcl_fname="psfgen.tcl", psf_fname="L", chain_id="L")  
 # Now we switch over into vmd...
